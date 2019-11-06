eff514f6bb3b3075c91ca14f89c48e3a043b3a3a
João Vitor Assumpção <jrvitor5@gmail.com>
2019-11-06 10:15:43 -0300

    Criação de Hooks

--- src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
+++ src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle-child',
   templateUrl: './lifecycle-child.component.html',
   styleUrls: ['./lifecycle-child.component.css']
 })
-export class LifecycleChildComponent implements OnInit {
-
-  constructor() { }
+export class LifecycleChildComponent implements OnInit, OnDestroy, OnChanges {
 
   @Input() name: string;
   @Input() age: number;
   @Input() food: string;
 
+  constructor() { 
+    console.log(this.name + " - Constructor");
+  }
 
   ngOnInit() {
+    console.log(this.name + " - ngOnInit");
+  }
+
+  ngOnDestroy(){
+    console.log(this.name + " - ngOnDestroy");
+  }
+
+  ngOnChanges(changes: SimpleChange){
+    console.log(this.name + " - ngOnChanges");
+  }
+
+  ngAfterContentInit(){
+    console.log(this.name + " - ngAfterContentInit");
+  }
+  ngAfterViewInit(){
+    console.log(this.name + " - ngAfterViewInit");
   }
 
 }
